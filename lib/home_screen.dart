4969eec5739885d65d6ff614e437681543bdf7a7
ashikahmedfahim <41462787+ashikahmedfahim@users.noreply.github.com>
2023-05-05 18:24:39 +0600

    home screen completed

---
+++ lib/home_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'Learn Flutter the fun way',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            child: const Text('Start Quiz'),
+          ),
+        ],
+      ),
+      // child: Image.asset('/assets/images/quiz-logo.png'),
+    );
+  }
+}
