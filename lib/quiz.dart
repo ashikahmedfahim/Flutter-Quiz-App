06d62c9cde7f901156d79e4088fe179063084e50
ashikahmedfahim <41462787+ashikahmedfahim@users.noreply.github.com>
2023-05-05 21:17:05 +0600

    code refactored

---
+++ lib/quiz.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/home_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({Key? key}) : super(key: key);
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const HomeScreen(),
+        ),
+      ),
+    );
+  }
+}
